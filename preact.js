03c28adcd466fac60f0af9a8c417fe03e183f4f2
Sasha Koss <koss@nocorp.me>
2017-06-30 16:19:46 +0200

    First working prototype

---
+++ preact.js
@@ -0,0 +1,7 @@
+module.exports = function (source) {
+  return `
+${source}
+var decss = require('decss/preact')
+exports.locals = decss(exports.locals)
+`
+}
---
+++ react.js
@@ -0,0 +1,7 @@
+module.exports = function (source) {
+  return `
+${source}
+var decss = require('decss/react')
+exports.locals = decss(exports.locals)
+`
+}
