aebdd1a07bf4d93435d0ae10f7f436a23449c2f0
Sasha Koss <koss@nocorp.me>
2017-08-01 13:05:27 +0300

    Prevent exception when the loading CSS has no selectors

--- preact.js
+++ preact.js
@@ -2,6 +2,6 @@ module.exports = function (source) {
   return `
 ${source}
 var decss = require('decss/preact')
-exports.locals = decss(exports.locals)
+exports.locals = decss(exports.locals || {})
 `
 }
--- react.js
+++ react.js
@@ -2,6 +2,6 @@ module.exports = function (source) {
   return `
 ${source}
 var decss = require('decss/react')
-exports.locals = decss(exports.locals)
+exports.locals = decss(exports.locals || {})
 `
 }
